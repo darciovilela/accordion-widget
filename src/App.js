65142a2ed970bc41006897e3d2046cc27cb3d1bc
Darcio Vilela <darcio@jp7.com.br>
2021-11-24 11:10:06 -0300

    Publish accordion component

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Accordion from './components/Accordion';
+
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a Js front end framework.',
+  },
+
+  {
+    title: 'Why use React?',
+    content: 'React is favorite among dev community.',
+  },
+
+  {
+    title: 'How do you use React?',
+    content: 'You use React by creating reusable components.',
+  },
+];
+
+// Component
+const myApp = () => {
+  return (
+    <div>
+      <Accordion items={items} />
+    </div>
+  );
+};
+
+export default myApp;
