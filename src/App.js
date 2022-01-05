28b439819ca306736b82b216ec450b859ddef298
Darcio Vilela <darcio@jp7.com.br>
2022-01-05 11:58:06 -0300

    Adding search widget

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Accordion from './components/Accordion';
+import Search from './components/Search';
 
 const items = [
   {
@@ -22,7 +23,7 @@ const items = [
 const MyApp = () => {
   return (
     <div>
-      <Accordion items={items} />
+      <Search />
     </div>
   );
 };
