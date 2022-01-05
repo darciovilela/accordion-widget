0731727ce96abac843abcb334e5e9d5b6c2fa065
Darcio Vilela <darcio@jp7.com.br>
2022-01-05 13:09:51 -0300

    Fix typo in wikipedia links

--- src/components/Search.js
+++ src/components/Search.js
@@ -29,7 +29,7 @@ const Search = () => {
         return (
             <div key={result.pageid} className='item'>
                 <div className='right floated content'>
-                    <a className='ui button' href={`https://en.wikipedia.org?cruid=${result.pageid}`}>Go</a>
+                    <a className='ui button' href={`https://en.wikipedia.org?curid=${result.pageid}`}>Go</a>
                 </div>
                 <div className='content'>
                     <div className='header'>
