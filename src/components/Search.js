0c08d678c9eb64d076cd92e9dff8ba9952f61fc0
Darcio Vilela <darcio@jp7.com.br>
2022-01-06 20:50:41 -0300

    Implement setTimeout and clearTimeout to limit API requests

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Component
 const Search = () => {
-    const [term, setTerm] = useState('programming');
+    const [term, setTerm] = useState('');
     const [results, setResults] = useState([]);
 
     useEffect(() => {
@@ -20,9 +20,17 @@ const Search = () => {
 
            setResults(data.query.search);
        };
-    if (term) {
-        search();
-    }
+
+        const timeoutId = setTimeout(() => {
+            if (term) {
+         search();
+        }
+        }, 500);
+
+        return () => {
+            clearTimeout(timeoutId);
+        }
+
     }, [term]);
     
     const renderedResults = results.map((result) => {
