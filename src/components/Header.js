d6d947d898e9cadb8ae1b6191553c7050bb61d76
Darcio Vilela <darcio@jp7.com.br>
2022-09-14 11:12:05 -0300

    Change Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,20 +4,7 @@ import Link from './Link';
 const Header = () => {
   return (
     <header className="App-header">
-      <div className="ui secondary pointing menu">
-        <Link href="/" className="item">
-          Accordion
-        </Link>
-        <Link href="/search" className="item">
-          Search
-        </Link>
-        <Link href="/dropdown" className="item">
-          Dropdown
-        </Link>
-        <Link href="/translate" className="item">
-          Translate
-        </Link>
-      </div>
+      <div className="logo">React Widgets</div>
     </header>
   );
 };
